cd69f3bfc547fa7472280d0e12714b96db7350c9
MuhamadNabila <44949192+muhamadnabila@users.noreply.github.com>
2018-11-24 02:02:47 +0700

    exercise-7

---
+++ exercise-7.js
@@ -0,0 +1,42 @@
+// 1. Menyusun baris bintang
+console.log ('PROGRAM START FROM HERE--');
+
+var rows1;
+for ( rows1 = 5 ; rows1 > 0 ; rows1-- ){
+    console.log ('*');
+}
+
+console.log ('PROGRAM START FROM HERE--');
+
+var rows2 = 5;
+var col ;
+var star = '';
+var col = rows2;
+
+for (rows2; rows2 > 0 ; rows2--){
+    while ( col <10 ){
+        star += '*'
+        col++;
+        
+    }
+    console.log ( star );
+}
+
+console.log ('PROGRAM START FROM HERE--');
+
+var rows2 = 5;
+var col ;
+var star = '';
+var col = rows2;
+
+for (rows2; rows2 > 0 ; rows2--){
+    while ( col <10 ){
+        star += '*'
+        col++;
+        console.log ( star );
+    
+    }
+}
+
+
+
